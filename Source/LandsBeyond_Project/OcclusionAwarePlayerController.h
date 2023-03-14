386d4d5bde1b8669ef3056603ada82ed2fbdd4e8
Alexis Harnois <alexisharnois@hotmail.com>
2023-03-14 12:58:31 -0400

    Camera culling

--- Source/LandsBeyond_Project/OcclusionAwarePlayerController.h
+++ Source/LandsBeyond_Project/OcclusionAwarePlayerController.h
@@ -1,14 +1,9 @@
-/**
-* !! NOTICE !!
-* Instructions: https://alfredbaudisch.com/blog/gamedev/unreal-engine-ue/unreal-engine-actors-transparent-block-camera-occlusion-see-through/
-*/
-
-// OcclusionAwarePlayerController.h
-// By Alfred Reinold Baudisch (https://github.com/alfredbaudisch)
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
 #include "GameFramework/PlayerController.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
@@ -18,77 +13,66 @@
 USTRUCT(BlueprintType)
 struct FCameraOccludedActor
 {
-  GENERATED_USTRUCT_BODY()
+	GENERATED_USTRUCT_BODY()
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		const AActor* Actor;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* StaticMesh;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-  const AActor* Actor;
-
-  UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-  UStaticMeshComponent* StaticMesh;
-  
-  UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-  TArray<UMaterialInterface*> Materials;
-
-  UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-  bool IsOccluded;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		TArray<UMaterialInterface*> Materials;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		bool IsOccluded;
 };
 
 /**
  * 
  */
 UCLASS()
-class DACHSHUNDSPA_API AOcclusionAwarePlayerController : public APlayerController
+class LANDSBEYOND_PROJECT_API AOcclusionAwarePlayerController : public APlayerController
 {
-  GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-  AOcclusionAwarePlayerController();
-
+	AOcclusionAwarePlayerController();
 protected:
-  // Called when the game starts
-  virtual void BeginPlay() override;
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	/** How much of the Pawn capsule Radius and Height
+	 * should be used for the Line Trace before considering an Actor occluded?
+	 * Values too low may make the camera clip through walls.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion|Occlusion",
+		meta = (ClampMin = "0.1", ClampMax = "10.0"))
+		float CapsulePercentageForTrace;
 
-  /** How much of the Pawn capsule Radius and Height
-   * should be used for the Line Trace before considering an Actor occluded?
-   * Values too low may make the camera clip through walls.
-   */
-  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera Occlusion|Occlusion",
-    meta=(ClampMin="0.1", ClampMax="10.0") )
-  float CapsulePercentageForTrace;
-  
-  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera Occlusion|Materials")
-  UMaterialInterface* FadeMaterial;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion|Materials")
+		UMaterialInterface* FadeMaterial;
+	UPROPERTY(BlueprintReadWrite, Category = "Camera Occlusion|Components")
+		class USpringArmComponent* ActiveSpringArm;
+	UPROPERTY(BlueprintReadWrite, Category = "Camera Occlusion|Components")
+		class UCameraComponent* ActiveCamera;
+	UPROPERTY(BlueprintReadWrite, Category = "Camera Occlusion|Components")
+		class UCapsuleComponent* ActiveCapsuleComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion")
+		bool IsOcclusionEnabled;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Occlusion|Occlusion")
+		bool DebugLineTraces;
 
-  UPROPERTY(BlueprintReadWrite, Category="Camera Occlusion|Components")
-  class USpringArmComponent* ActiveSpringArm;
-
-  UPROPERTY(BlueprintReadWrite, Category="Camera Occlusion|Components")
-  class UCameraComponent* ActiveCamera;
-
-  UPROPERTY(BlueprintReadWrite, Category="Camera Occlusion|Components")
-  class UCapsuleComponent* ActiveCapsuleComponent;
-
-  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera Occlusion")
-  bool IsOcclusionEnabled;
-
-  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera Occlusion|Occlusion")
-  bool DebugLineTraces;
-  
 private:
-  TMap<const AActor*, FCameraOccludedActor> OccludedActors;
-  
-  bool HideOccludedActor(const AActor* Actor);
-  bool OnHideOccludedActor(const FCameraOccludedActor& OccludedActor) const;
-  void ShowOccludedActor(FCameraOccludedActor& OccludedActor);
-  bool OnShowOccludedActor(const FCameraOccludedActor& OccludedActor) const;
-  void ForceShowOccludedActors();
+	TMap<const AActor*, FCameraOccludedActor> OccludedActors;
 
-  __forceinline bool ShouldCheckCameraOcclusion() const
-  {
-    return IsOcclusionEnabled && FadeMaterial && ActiveCamera && ActiveCapsuleComponent;
-  }
-  
+	bool HideOccludedActor(const AActor* Actor);
+	bool OnHideOccludedActor(const FCameraOccludedActor& OccludedActor) const;
+	void ShowOccludedActor(FCameraOccludedActor& OccludedActor);
+	bool OnShowOccludedActor(const FCameraOccludedActor& OccludedActor) const;
+	void ForceShowOccludedActors();
+	__forceinline bool ShouldCheckCameraOcclusion() const
+	{
+		return IsOcclusionEnabled && FadeMaterial && ActiveCamera && ActiveCapsuleComponent;
+	}
+
 public:
-  UFUNCTION(BlueprintCallable)
-  void SyncOccludedActors();
+	UFUNCTION(BlueprintCallable)
+		void SyncOccludedActors();
 };
